dd6fa6c2981f8ba6b10dd95f2c2505fff076637a
shehzad-kore <sibrahim@korewireless.com>
2022-09-25 18:24:01 +0530

    update token

--- tokengen/token.go
+++ tokengen/token.go
@@ -160,11 +160,11 @@ func main() {
 	      log.Fatal(err)
 	  }*/
 	log.Println("[main] Sign String")
-	tokenString, err := createJWT(*projectID, *privateKey, "RS256", 525960) //token.SignedString(key)
+	_, err = createJWT(*projectID, *privateKey, "RS256", 525960) //token.SignedString(key)
 	if err != nil {
 		log.Fatal(err)
 	}
-	opts.SetPassword(tokenString)
+	opts.SetPassword("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJteS1pb3QtMzU2MzA1IiwiZXhwIjoxNjk1NjY3OTc0LCJpYXQiOjE2NjQxMTAzNzR9.T_kzjb2mQVtF_0J9zY7QuJiY8z5sd8-VNN8XW06xo1CGQvpjYnOcfVs0tfh6t8VWDZq5PndcbNTNCybZbJd4Dhzxw_Rz-6PJoFqe9HisIl7xyRNanxzVEeeBE-3SSmJRSPTGYjx6VHZU2xRYCNmXSi0UdLPi6P43-TdK3gPZDR57CJQbbGUdVSotVAz9tbETNBdthZK6tpw8o8EgKpsBfKKOzNmXYAtt9wHuoPSI_HlFSviMMEEYZuC8Ss3xJ6nGWJuQEY6G4epsrnjxneT3fHGcjflI-if4FmdRmxmcvCQBrZd2UGvylJTK96Ir3WQfcJbQdT2n9Fc7VVifYR3Lzw")
 	// Incoming
 	opts.SetDefaultPublishHandler(func(client MQTT.Client, msg MQTT.Message) {
 		fmt.Printf("[handler] Topic: %v\n", msg.Topic())
